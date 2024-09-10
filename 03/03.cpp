dff58a511682ce06d8dc79089cbaf5940c6464fd
shinsouhitoshi1203 <sinnohsatoshi.1203@gmail.com>
2024-09-10 13:47:41 +0700

    reset ./03

--- 03/03.cpp
+++ 03/03.cpp
@@ -1,12 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-main() {
-    int s1,s2,s3;
-    float a,b,c;
-    cin >> s1 >> s2 >> s3;
-    float ar = (float)s1/s2;
-    c = sqrt((float)s3/ar);
-    b = (float)c*ar; a = (float)s1/b;
-    cout << a << " " << b << " " << c;
+int main() {
+    cout << "Hello world";
+    return 0;
 }
